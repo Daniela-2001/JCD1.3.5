d24e06063f83de834ee54ead23eac5ff6c1c318e
Daniela Duarte <danielatavaresd@gmail.com>
2023-12-31 00:18:42 +0000

    JCD1.3.5

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace JCD1._3._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número de linhas ");
+
+            int linhas = Convert.ToInt32(Console.ReadLine());
+
+            string arvore = "*";
+
+            int i = 1;
+
+            do
+
+            {
+                Console.WriteLine(new String('*', i )); // Aqui o i vai multiplicar o valor do caractere (a quantidade de * corresponde com o número da linha)
+                i++; // Aqui ele sempre somará mais 1
+            }while (i < linhas);
+        }
+    }
+}
